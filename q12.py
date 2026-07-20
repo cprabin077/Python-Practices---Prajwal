cca4793463062cb5b4c5000f168f3116b535d1b6
= <kushmiprabin123@gmail.com>
2026-07-20 22:42:04 +0545

    feat: alternative method for factorial of a  number

--- q12.py
+++ q12.py
@@ -9,4 +9,18 @@ else:
     result = math.factorial(num)
     print(f"Factorial of {num} = ", result)
 
-    
+
+print("------------"*5)
+# Alternative Method:
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+else:
+    factorial = 1
+
+    for i in range(1, num + 1):
+        factorial *= i
+
+    print(f"Factorial of {num} = ", result)
