7c29c2a1d46e6433b3eb5d229cb033bd103d7a04
= <kushmiprabin123@gmail.com>
2026-07-20 22:35:52 +0545

    feat: alternative method for square root

--- q11.py
+++ q11.py
@@ -6,3 +6,16 @@ if num < 0:
     print("Square root of a negative number is not possible.")
 else: 
     print(f"The square of {num} is: ", num**0.5) # root = pow(1/2) => pow(0.5)
+
+print("-------------------"*5)
+# Alternative method
+import math
+
+num = float(input("Enter a number: "))
+
+# Check for negative number
+if num < 0:
+    print("Square root of a negative number is not possible.")
+else:
+    result = math.sqrt(num)
+    print(f"The square of {num} is: ", result)
