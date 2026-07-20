459458ca9e776ccd2b68f1e2ecce5716c8e0e4c2
= <kushmiprabin123@gmail.com>
2026-07-20 22:53:31 +0545

    feat: Round a decimal number upward using ceil()

---
+++ q14.py
@@ -0,0 +1,10 @@
+# 14.	Round a decimal number upward using ceil().
+
+import math
+
+num = float(input("Enter a decimal number: "))
+
+
+result = math.ceil(num) # Round upward
+
+print("Rounded value =", result)
