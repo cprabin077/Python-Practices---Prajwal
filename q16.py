f58cabcd643be681c3220f586bc165ad9bd9aa2e
= <kushmiprabin123@gmail.com>
2026-07-21 23:35:55 +0545

    feat: absolute value of a negative number using fabs()

---
+++ q16.py
@@ -0,0 +1,9 @@
+# 16.	Find the absolute value of a negative number using fabs().
+
+import math
+
+num = float(input("Enter a decimal number: "))
+
+result = math.fabs(num) # Absolute value i.e. -10 => 10
+
+print("Absolute value =", result)
