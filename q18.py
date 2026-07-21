4d61e22e887e3fc098b10c0bcb99835c2e7d3d70
= <kushmiprabin123@gmail.com>
2026-07-21 23:41:19 +0545

    feat: value of 2 raised to the power 10 using pow()

---
+++ q18.py
@@ -0,0 +1,8 @@
+# 18.	Calculate the value of 2 raised to the power 10 using pow().
+
+base = int(input("Enter the base: "))
+exponent = int(input("Enter the exponent: "))
+
+result = pow(base, exponent)
+
+print("Result =", result)
