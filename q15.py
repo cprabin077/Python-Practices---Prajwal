84be570156951f4bf61c01464e83585015162777
= <kushmiprabin123@gmail.com>
2026-07-21 23:31:32 +0545

    feat: Round a decimal number downward using floor()

---
+++ q15.py
@@ -0,0 +1,9 @@
+# 15.	Round a decimal number downward using floor().
+
+import math
+
+num = float(input("Enter a decimal number: "))
+
+result = math.floor(num) # Round upward
+
+print("Rounded value =", result)
