d4ac77f8d3a2b23e8914fbd4b6882b1321b97053
= <kushmiprabin123@gmail.com>
2026-07-21 23:47:46 +0545

    feat: values of math.pi and math.e

---
+++ q20.py
@@ -0,0 +1,6 @@
+# 20.	Display the values of math.pi and math.e.
+
+import math
+
+print("Value of pi =", math.pi) # Value of pi = 3.141592653589793
+print("Value of e =", math.e) # Value of e = 2.718281828459045
