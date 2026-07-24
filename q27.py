9ec2115a228fdfdb0d5cd3e6f70d865842b0def3
= <kushmiprabin123@gmail.com>
2026-07-24 13:26:28 +0545

    feat: Alternative method to to shuffle a list of names

--- q27.py
+++ q27.py
@@ -8,3 +8,17 @@ random.shuffle(names)
 
 print("Shuffled Names:", names)
 
+print("-------------"*5)
+
+# Alternative Method: Using User Input
+
+import random
+
+# Input student names
+names = input("Enter student names separated by commas: ").split(",")
+
+names = [name.strip() for name in names]
+
+random.shuffle(names)
+
+print("Shuffled Names:", names)
