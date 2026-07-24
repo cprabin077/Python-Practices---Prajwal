a43fca7d72add5837349bba47569462de97500c3
= <kushmiprabin123@gmail.com>
2026-07-24 13:39:09 +0545

    feat: lottery winner selection program

---
+++ q30.py
@@ -0,0 +1,9 @@
+# 30.	Create a simple lottery winner selection program.
+
+import random
+
+participants = ["Prabin", "Ruchi", "Rekha", "Sagar", "Prajwal", "Bibek", "Depti"]
+
+winner = random.choice(participants)
+
+print("🎉 Lottery Winner:", winner)
