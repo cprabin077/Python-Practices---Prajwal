71fb29220d1400453848a06a32eee6df6a3a969f
= <kushmiprabin123@gmail.com>
2026-07-24 13:20:15 +0545

    feat: Alternative method to Pick a random student from a list

--- q26.py
+++ q26.py
@@ -9,4 +9,20 @@ student = random.choice(students)
 
 print("Selected Student:", student)
 
+print("-------------"*5)
+
+# Alternative Method: Using User Input
+
+import random
+
+# Input student names
+students = input("Enter student names separated by commas: ").split(",")
+
+# Remove extra spaces
+students = [student.strip() for student in students]
+
+# Pick a random student
+print("Selected Student:", random.choice(students))
+
+
 
