4b52659e836760df509d2ed55e1e0bdb4d3c3a80
= <kushmiprabin123@gmail.com>
2026-07-24 13:31:22 +0545

    feat: Alternative method:  Select 3 unique winners from a list of participants

--- q28.py
+++ q28.py
@@ -7,3 +7,16 @@ participants = ["Prabin", "Ruchi", "Rekha", "Sagar", "Prajwal", "Bibek", "Depti"
 winners = random.sample(participants, 3)
 
 print("Winners:", winners)
+
+print("--------------"*5)
+
+# Alternative Method: Using User Input
+import random
+
+participants = input("Enter participant names separated by commas: ").split(",")
+
+participants = [name.strip() for name in participants]
+
+winners = random.sample(participants, 3)
+
+print("Winners:", winners)
