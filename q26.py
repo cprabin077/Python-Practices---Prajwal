99dbda630a133bce0f07e39e9d46c95447eb8151
= <kushmiprabin123@gmail.com>
2026-07-24 13:17:49 +0545

    feat: Pick a random student from a list

---
+++ q26.py
@@ -0,0 +1,12 @@
+# 26.	Pick a random student from a list.
+
+import random
+
+# List of students
+students = ["Prabin", "Ruchi", "Rekha", "Sagar", "Prajwal", "Bibek"]
+
+student = random.choice(students)
+
+print("Selected Student:", student)
+
+
